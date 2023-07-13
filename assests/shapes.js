e5cd76493c855ab9b804fd71260cedf6599204ea
adamjfox <adamjeffersonfox@gmail.com>
2023-07-13 15:21:39 -0700

    how do you make a circle

--- assests/shapes.js
+++ assests/shapes.js
@@ -1,7 +1,29 @@
 // class for shape?
-
+    class Shape{
+        constructor(){
+            this.color=''
+            }
+        setColor(color){
+            this.color=(color);
+        }
+    }
+//each shape should return a polygon with color input
 //class for circle
-
+class Circle extends Shape{
+    render(){
+        return
+    }
+}
 //class for triangle
+class Triangle extends Shape{
+    render(){
+        return
+    }
+}
 
 //class for square
+class Square extends Shape{
+    render(){
+        return
+    }
+}
