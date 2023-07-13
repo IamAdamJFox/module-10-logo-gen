29e3f21150402d82cdac9a82b4e9f4ce70f186df
adamjfox <adamjeffersonfox@gmail.com>
2023-07-13 15:00:15 -0700

    index start

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 //import
 
 //required packages
-
+const fs = require("fs");
+const inquier = require("inquirer");
 //array questions
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "",
+    },
+    {
+        type: "input",
+        name: "text",
+        message: "",
+    },
+    {
+        type: "input",
+        name: "text",
+        message: "",
+    }
+    {
+        type: "input",
+        name: "text",
+        message: "",
+    },
+    {
+        type: "input",
+        name: "text",
+        message: "",
+    },
+    {
+        type: "input",
+        name: "text",
+        message: "",
+    }
+
+]
 
 //function to write file
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (error) {
+            return console.log(err);
+        }
+        
+        console.log("logo made successfully")
+    });
+
+}
